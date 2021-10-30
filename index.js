65104fff823390efd07b0426d2ef9df1a0f06ec3
vcuencas <vcs04@hotmail.com>
2021-10-30 12:20:48 -0400

    testing output

--- index.js
+++ index.js
@@ -1,2 +1,31 @@
-const username = process.argv[2].split('=')[1]
-console.log(`Hello, ${username}`)
+const connectionString = 'mongodb+srv://m001-student:m001-mongodb-basics@eye4fish.eneq6.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+const mongoose = require('mongoose')
+const userSchema = require('./userSchema.js')
+const User = mongoose.model('user', userSchema, 'user')
+
+async function createUser(username) {
+  return new User({
+    username,
+    created: Date.now()
+  }).save()
+}
+
+async function findUser(username) {
+  return await User.findOne({ username })
+}
+
+;(async () => {
+  const connector = mongoose.connect(connectionString)
+  const username = process.argv[2].split('=')[1]
+
+  let user = await connector.then(async () => {
+    return findUser(username)
+  })
+
+  if (!user) {
+    user = await createUser(username)
+  }
+
+  console.log(user)
+  process.exit(0)
+})()
