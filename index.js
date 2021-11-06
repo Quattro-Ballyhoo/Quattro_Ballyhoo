7f90bf555c95ff3daa1c639c53cfbce7b9fbf4c0
vcuencas <vcs04@hotmail.com>
2021-11-06 10:20:33 -0400

    imported json to data

--- index.js
+++ index.js
@@ -1,31 +1,41 @@
 const connectionString = 'mongodb+srv://m001-student:m001-mongodb-basics@eye4fish.eneq6.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
 const mongoose = require('mongoose')
 const userSchema = require('./userSchema.js')
+var assert = require('assert')
 const User = mongoose.model('user', userSchema, 'user')
 
-async function createUser(username) {
-  return new User({
-    username,
-    created: Date.now()
-  }).save()
-}
+var output = require('./myOutputFile.js')
 
-async function findUser(username) {
-  return await User.findOne({ username })
-}
+// async function createUser(username) {
+//   return new User({
+//     username,
+//     created: Date.now()
+//   }).save()
+// }
+//
+// async function findUser(username) {
+//   return await User.findOne({ username })
+// }
 
-;(async () => {
-  const connector = mongoose.connect(connectionString)
-  const username = process.argv[2].split('=')[1]
+const connector = mongoose.connect(connectionString)
+var species = mongoose.model('species', userSchema)
+species.collection.insertMany(output, function(err,r) {
+    assert.equal(null,err);
 
-  let user = await connector.then(async () => {
-    return findUser(username)
-  })
+})
+mongoose.disconnect();
+// ;(async () => {
+//   const connector = mongoose.connect(connectionString)
+//   const username = process.argv[2].split('=')[1]
+//
+//   let user = await connector.then(async () => {
+//     return findUser(username)
+//   })
+//
+//   if (!user) {
+//     user = await createUser(username)
+//   }
 
-  if (!user) {
-    user = await createUser(username)
-  }
-
-  console.log(user)
-  process.exit(0)
-})()
+  //console.log(user)
+  //process.exit(0)
+//})()
