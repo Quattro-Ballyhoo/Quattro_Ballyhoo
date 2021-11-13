d7e4ded84dbd5c1ec858dbb10af5f1a84c6e2184
vcuencas <vcs04@hotmail.com>
2021-11-13 10:43:51 -0500

    made changes to index.js for query

--- index.js
+++ index.js
@@ -1,41 +1,36 @@
-const connectionString = 'mongodb+srv://m001-student:m001-mongodb-basics@eye4fish.eneq6.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
-const mongoose = require('mongoose')
-const userSchema = require('./userSchema.js')
-var assert = require('assert')
-const User = mongoose.model('user', userSchema, 'user')
-
-var output = require('./myOutputFile.js')
-
-// async function createUser(username) {
-//   return new User({
-//     username,
-//     created: Date.now()
-//   }).save()
-// }
-//
-// async function findUser(username) {
-//   return await User.findOne({ username })
-// }
-
-const connector = mongoose.connect(connectionString)
-var species = mongoose.model('species', userSchema)
-species.collection.insertMany(output, function(err,r) {
-    assert.equal(null,err);
-
-})
-mongoose.disconnect();
-// ;(async () => {
-//   const connector = mongoose.connect(connectionString)
-//   const username = process.argv[2].split('=')[1]
-//
-//   let user = await connector.then(async () => {
-//     return findUser(username)
-//   })
-//
-//   if (!user) {
-//     user = await createUser(username)
-//   }
-
-  //console.log(user)
-  //process.exit(0)
-//})()
+const mongoose = require("mongoose");
+const connectionString = "mongodb+srv://m001-student:m001-mongodb-basics@eye4fish.eneq6.mongodb.net/Eye4Fish?retryWrites=true&w=majority";
+const fishiesSchema = require("./fishiesSchema.js");
+var output = require("./myOutputFile.js");
+const prompt = require('prompt');
+​
+mongoose.connect(connectionString, function(error, result)
+{
+  if (error) {
+    console.log("Fail to connect");
+  }
+  if (result) {
+    console.log("Connection worked");
+  }
+});
+​
+prompt.start();
+​
+prompt.get(['Species_name'], function(err,result) {
+  if (err) {return onErr(err);}
+  return queryData(result);
+});
+​
+function onErr(err) {
+  console.log(err);
+  return 1;
+};
+​
+function queryData(result) {
+  const species = mongoose.model("species", fishiesSchema);
+​
+  let Species = species.find({latitude: result.Species_name}).then(
+    err => console.log(err),
+    species => console.log("test")
+  );
+};
